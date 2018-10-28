54513392ef6288b702c3c2ea4a513bfa7d4849c5
pavel <pavelametov@gmail.com>
2018-10-28 23:24:32 +0300

    add Polynom exception

---
+++ Polynom/src/PolynomException/ValueTypeException.py
@@ -0,0 +1,6 @@
+from src.PolynomException.PolynomExceptionBase import PolynomExceptionBase
+
+
+class ValueTypeException(PolynomExceptionBase):
+    def __init__(self, message: str = ''):
+        super(message)
