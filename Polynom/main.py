76fa4bc7f7be163601bdfebd94295796b1aa106d
pavel <pavelametov@gmail.com>
2018-10-29 19:59:34 +0300

    main.py with examples added

---
+++ Polynom/main.py
@@ -0,0 +1,29 @@
+from src.Polynom import Polynom
+from src.PolynomException.PolynomExceptionBase import PolynomExceptionBase
+
+if __name__ == '__main__':
+    try:
+        polynom1 = Polynom({2: 1, 1: 2, 0: -1})
+        polynom2 = Polynom({1: 2, 0: 3})
+        polynom3 = Polynom({3: 2, 2: 3})
+
+        print(f'polynom1: {polynom1}')
+        print(f'polynom2: {polynom2}')
+        print(f'polynom3: {polynom3}')
+
+        print(f'len: {len(polynom1)}')
+        print(f'len: {len(polynom3)}')
+        print(f'pow: {pow(polynom3, 2)}')
+        print(f'superposition 1->2: {Polynom.superposition(polynom1, polynom2)}')
+        print(f'superposition 2->1: {Polynom.superposition(polynom2, polynom1)}')
+
+        print(f'polynom1 + polynom2: {polynom1 + polynom2}')
+        print(f'polynom1 - polynom2: {polynom1 - polynom2}')
+        print(f'polynom1 * polynom2: {polynom1 * polynom2}')
+        print(f'polynom1 * 3: {polynom1 * 3}')
+        print(f'differential polynom1: {polynom1.differential}')
+        print(f'integral polynom1: {polynom1.integral}')
+    except PolynomExceptionBase as error:
+        print(error.message)
+    except Exception as error:
+        print(error)
